ee0de242c04d7c944130ac56c4db905cb1a0f4b4
gera <gerardo.ascencio.r@gmail.com>
2022-11-14 21:55:29 -0600

    Progreso del 14 de noviembre

---
+++ Fase 2/Sesión_2/Post_work.R
@@ -0,0 +1,17 @@
+"1. Inspecciona el DataSet iris_meaniris` disponible directamente en R.
+Identifica las variables que contiene y su tipo, asegúrate de que no hayan datos
+faltantes y que los datos se encuentren listos para usarse."
+
+"2. Crea una gráfica de puntos que contenga Sepal.Lenght en el eje horizontal,
+Sepal.Width en el eje vertical, que identifique Species por color y que el
+tamaño de la figura está representado por Petal.Width. Asegúrate de que la
+geometría contenga shape = 10 y alpha = 0.5"
+
+"3. Crea una tabla llamada iris_mean que contenga el promedio de todas las
+variables agrupadas por Species."
+
+"4. Con esta tabla, agrega a tu gráfica anterior otra geometría de puntos para
+agregar los promedios en la visualización. Asegúrate que el primer argumento de
+la geometría sea el nombre de tu tabla y que los parámetros sean shape = 23,
+size = 4, fill = \"black\" y stroke = 2. También agrega etiquetas, temas y los
+cambios necesarios para mejorar tu visualización."
---
+++ Fase 2/Sesión_2/Reto_1.R
@@ -0,0 +1,39 @@
+#Desarollo
+
+# En este reto vamos a practicar nuestra habilidad en R para leer datos usando
+# que ya hemos utilizado y agregando nuevas a nuestro repertorio
+
+# Supongamos que estás interesado en utilizar todos los datos disponibles de la
+# liga española 2019/2020 y formar un DataFrame único de los siguientes archivos:
+  
+u1011 <- "https://www.football-data.co.uk/mmz4281/1011/SP1.csv"
+u1112 <- "https://www.football-data.co.uk/mmz4281/1112/SP1.csv"
+u1213 <- "https://www.football-data.co.uk/mmz4281/1213/SP1.csv"
+u1314 <- "https://www.football-data.co.uk/mmz4281/1314/SP1.csv"
+
+  #1 Crea una lista con los nombres de las variables anteriores
+
+lista.archivos <- list(u1011, u1112, u1213, u1314)
+  
+  # 2 Con ayuda de la función lapply() lee todos los .csv de lista.archivos.
+  # Usa help('lapply') para saber más de esta función. ¿Qué tipo de objeto
+  # regresa lapply?
+  # De acuerdo a la ayuda, la función lapply retorna una lista de la misma
+  # longitud de X, y a cada elemento le aplica lo que diga la función FUN
+  help('lapply')
+  lista.csv <- lapply(lista.archivos, read.csv, TRUE)
+  
+  # 3 Ejecuta la siguiente función para sólo tomar un rango de variables. Más
+  # adelante en esta sesión veremos la función select()
+  # library(dplyr) (para correr con "select")
+  lista.datos <- lapply(lista.csv, select, Date:FTR)
+
+
+  # 4 Utiliza la función do.call() para repetir una función que te permite juntar
+  # por filas todos los elementos de lista.datos. Usa help('do.call') para saber
+  # más de esta función.
+  help('do.call')
+
+  df <- do.call(rbind, lista.datos)
+  
+  head(df)
