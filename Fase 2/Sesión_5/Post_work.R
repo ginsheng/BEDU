b8f1f77a829d0861e9bfa806ba7e82d60f1d04f1
ginsheng <tsen.tso@gmail.com>
2022-12-11 17:22:31 +0000

    Avance al corriente con episodio 5

---
+++ Fase 2/Sesión_5/Post_work.R
@@ -0,0 +1,24 @@
+"El data frame iris contiene información recolectada por Anderson sobre 50
+flores de 3 especies distintas (setosa, versicolor y virginca), incluyendo
+medidas en centímetros del largo y ancho del sépalo así como de los pétalos."
+
+"Estudios recientes sobre las mismas especies muestran que:"
+  
+"I. En promedio, el largo del sépalo de la especie setosa (Sepal.Length) es
+igual a 5.7 cm"
+
+"II. En promedio, el ancho del pétalo de la especie virginica (Petal.Width) es
+menor a 2.1 cm"
+
+"III. En promedio, el largo del pétalo de la especie virgínica es 1.1 cm más
+grande que el promedio del largo del pétalo de la especie versicolor."
+
+"IV. En promedio, no existe diferencia en el ancho del sépalo entre las 3
+especies."
+
+"Utilizando pruebas de inferencia estadística, concluye si existe evidencia
+suficiente para concluir que los datos recolectados por Anderson están en línea
+con los nuevos estudios."
+
+"Utiliza 99% de confianza para todas las pruebas, en cada caso realiza el
+planteamiento de hipótesis adecuado y concluye"
